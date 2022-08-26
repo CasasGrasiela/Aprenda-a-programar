34ee708234a5a10d4f02bb2dedf6220ae0eac493
CasasGrasiela <casas@gmail.com>
2022-08-26 16:17:14 -0300

    pratica-1 template strings

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -2,8 +2,8 @@ nome = "Grasiela Casas"
 email = "casas.gr@gmail.com"
 idade = 39
 profissao = "Ecologa pesquisadora"
+sonho = "Tahiti"
+console.log(nome, email, idade, profissao, sonho)
 
-console.log(nome, email, idade, profissao)
-
-
+console.log(`Olá ${nome}, Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${sonho}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
 
