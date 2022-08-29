93c817956ff977ede8f62a0cfe2a9e0b28d0be1a
CasasGrasiela <casas@gmail.com>
2022-08-29 18:48:28 -0300

    loop pratica 1

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,12 @@
+let contador = 1
+while (contador <= 9) {
+    console.log (contador)
+    contador = contador + 1
+} 
+
+let n = 9 //esse jeito peguei da resolucao. Coloquei tentando assim na linha 11 i=i++ e deu erro
+let i = 1
+while (i <= n) {
+    console.log(i)
+    i++
+} 
