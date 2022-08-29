677b62ab726fcdc1babc6e0809c628ad2e79dd2c
CasasGrasiela <casas@gmail.com>
2022-08-29 19:53:04 -0300

    numeros impares

---
+++ tarefa-dia-3/pratica-3.js
@@ -0,0 +1,19 @@
+/* So nao sei pq meu codigo parou no 17 e não 15*/
+const n = 16
+let i=0
+
+while (i<= n) {
+	i++
+	if (i %2 !=0)
+	console.log (i)
+}
+
+
+/* peguei da Resolucao */
+const n = 16 // aí o const novamente! :)
+let i = 1
+
+while (i <= n) {
+    console.log(i)
+    i += 2 // que é a mesma coisa que i = i + 2
+}
